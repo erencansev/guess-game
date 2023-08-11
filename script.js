30799ef4e13df4e6c9a1ca015d184bb390ab58d6
Muhammed Eren Cansev <cansev@Muhammed-Air.home>
2023-08-11 16:43:08 +0300

    feat:random number generation provided

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 "use strict";
-
+/* 
 document.querySelector(".message").textContent = "Correct Number";
 document.querySelector(".number").textContent = 13;
 document.querySelector(".score").textContent = 10;
 
 console.log(document.querySelector(".guess").value);
-document.querySelector(".guess").value = 23;
+document.querySelector(".guess").value = 23; */
+
+/* Returns a decimal number between zero and one */
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = number;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "🔴 No Number";
+  }
+});
