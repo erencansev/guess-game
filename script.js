4b23b8c2a897b6e791db92a211025f995d47ddf6
cansev <mee.cansev@gmail.com>
2023-08-11 23:01:29 +0300

    feat:implementing highscore

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 "use strict";
-/* 
-document.querySelector(".message").textContent = "Correct Number";
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
-
-console.log(document.querySelector(".guess").value);
-document.querySelector(".guess").value = 23; */
-
-/* Returns a decimal number between zero and one */
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -22,6 +14,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "🔥 Correct Number";
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 too high!";
